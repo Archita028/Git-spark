d1c79375980c38aa3194211d39ba91ba6cee89ca
Archita028 <architasahoo333@gmail.com>
2025-03-24 00:04:38 +0530

    commit

---
+++ spark.java
@@ -0,0 +1,11 @@
+public class spark{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+    public void print(){
+        System.out.println("Hello World");
+    }
+    public void print2(){
+        System.out.println("Hello World");
+    }
+}
